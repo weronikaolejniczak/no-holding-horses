fbfbb8e1b33ad501632984c7371761dc9b33e5ed
Weronika Olejniczak <weronika.olejniczak@rsqtechnologies.com>
2021-11-18 12:15:06 +0100

    deploy app and fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { Races } from 'pages/Races';
 import { Race } from 'pages/Race';
